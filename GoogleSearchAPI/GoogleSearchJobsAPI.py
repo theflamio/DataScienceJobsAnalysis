7db34bf2f74be384bf3d60e6aa37ef0d7a2a9f51
Flemming Christensen <flc@deif.com>
2022-11-28 22:27:27 +0100

    Replaced the scrapper modul with and API
    100 request each month

---
+++ GoogleSearchAPI/GoogleSearchJobsAPI.py
@@ -0,0 +1,21 @@
+from serpapi import GoogleSearch
+import json
+
+params = {
+    "api_key": "8452f12ed3998f8207bbd3063f25e8517b5231966f4fa2fa71e64b3ddf9be75d",
+    "device": "desktop",
+    "engine": "google_jobs",
+    "google_domain": "google.com",
+    "q": "data scientist"
+}
+
+search = GoogleSearch(params)
+results = search.get_dict()
+jobs_results = results["jobs_results"]
+
+print(type(jobs_results))
+
+print(jobs_results)
+
+for job in jobs_results:
+  print(job)

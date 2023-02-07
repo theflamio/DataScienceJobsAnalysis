abeab7ed73fbeb272c2119325b4e75725376a8d5
Flemming Christensen <flamekfor@gmail.com>
2023-02-07 22:40:33 +0100

    Added unittest

--- pytest.py
+++ pytest.py
@@ -1,9 +1,17 @@
-import pytest
+import sum_of_two
+
+from unittest import TestCase
 
 
-def test_func_fast():
-    pass
+class TestSumOfTwoNumbers(TestCase):
+    """Class for testing sum of two numbers module"""
 
-@pytest.mark.slow
-def test_func_slow():
-    pass
+
+    def test_good(self):
+        """Function for testing sum of two numbers module good outcome"""
+        self.assertEqual(sum_of_two.sum_numbers(1,1),2)
+
+
+    def test_bad(self):
+        """Function for testing sum of two numbers module bad outcome"""
+        self.assertNotEqual(sum_of_two.sum_numbers(1,2),2)
---
+++ sum_of_two.py
@@ -0,0 +1,6 @@
+''' Sum of two numbers Module'''
+
+
+def sum_numbers(first, second):
+    """function sum of two numbers module"""   
+    return first + second

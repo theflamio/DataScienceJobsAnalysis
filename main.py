613f4c0b75b60d685695217467fa3d0d16baeb98
Flemming Christensen <flamekfor@gmail.com>
2023-02-07 23:01:42 +0100

    Fixed it back to pytest

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-
+''' Main module '''
 if __name__ == '__main__':
     print("Hello World")
---
+++ test_sum_of_two.py
@@ -0,0 +1,15 @@
+''' Unittest of sum of two'''
+
+import sum_of_two
+
+import test_sum_of_two
+
+
+def test_good():
+    """Function for testing sum of two numbers module good outcome"""
+    assert sum_of_two.sum_numbers(1,1) == 2
+
+
+def test_bad():
+    """Function for testing sum of two numbers module bad outcome"""
+    assert sum_of_two.sum_numbers(1,2) != 2
